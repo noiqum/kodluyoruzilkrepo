20bf70232d3a738317d74fbfecde66ca088d4dc4
onur <onurcoskun99@gmail.com>
2021-12-26 14:17:34 +0300

    moduler system

---
+++ circle/circle.js
@@ -0,0 +1,10 @@
+export const circleArea=(r)=>{
+    const area=Math.PI * Math.pow(r,2)
+    return console.log(`yarıçapı ${r} olan dairenin alanı =`,area)
+}
+
+export const circleCircumference=(r)=>{
+    const cevre=2 * Math.PI * r;
+    console.log(`yarıçapı ${r} olan dairenin çevresi=`,cevre)
+}
+
